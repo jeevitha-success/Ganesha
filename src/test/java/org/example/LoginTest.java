afd78cc89df5df056cd72a79838376f40c44581a
jeevitha-success <jeevithatilak@gmail.com>
2026-03-09 16:15:47 +0530

    Added headless Chrome Selenium Code

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,16 +1,28 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
         @Test
         public void testGoogle() {
-            WebDriver driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            WebDriverManager.chromedriver().setup();
+
+            WebDriver driver = new ChromeDriver(options);
+
             driver.get("https://www.google.com");
-            System.out.println("Title: " + driver.getTitle());
+            System.out.println(driver.getTitle());
+
             driver.quit();
         }
     }
